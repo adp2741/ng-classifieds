7b1058b2bfe2b0e928095b99756dea094d02f928
Andrew Perry <adp2741@gmail.com>
2017-05-17 09:28:07 -0400

    More work with CRUD

--- components/classifiedsController.js
+++ components/classifiedsController.js
@@ -2,11 +2,12 @@
 	
 	"use strict";
 
-	angular.module("ngClassifieds").controller("classifiedsCtrl", function(classifiedsFactory, $mdSidenav, $mdToast) {
+	angular.module("ngClassifieds").controller("classifiedsCtrl", function(classifiedsFactory, $mdSidenav, $mdToast, $mdDialog) {
 		var self = this;
 
 		classifiedsFactory.getClassifieds().then(function(classifieds) {
 			self.classifieds = classifieds.data;
+			self.categories = getCategories(self.classifieds);
 		});
 
 		var contact = {
@@ -29,17 +30,55 @@
 				self.classifieds.push(classified);
 				self.classified = {};
 				self.closeSidebar();
-				$mdToast.show(
-					$mdToast.simple()
-						.content("Classified Saved!")
-						.position("top, right")
-						.hideDelay(3000)
-				);
+				showToast("Classified Saved!");
 			}
 		};
 
-		self.editClassified = function() {
+		self.editClassified = function(classified) {
 			self.editing = true;
+			self.openSidebar();
+			self.classified = classified;
+		};
+
+		self.saveEdit = function() {
+			self.editing = false;
+			self.classified = {};
+			self.closeSidebar();
+			showToast("Classified Edited!");
+		};
+
+		self.deleteClassified = function(event, classified) {
+			var confirm = $mdDialog.confirm()
+				.title("Are you sure you want to delete " + classified.title + "?")
+				.ok("Yes")
+				.cancel("No")
+				.targetEvent(event);
+			$mdDialog.show(confirm).then(function() {
+				var index = self.classifieds.indexOf(classified);
+				self.classifieds.splice(index, 1);
+			}, function() {
+
+			});
+		};
+
+		function showToast(message) {
+			$mdToast.show(
+				$mdToast.simple()
+					.content(message)
+					.position("top, right")
+					.hideDelay(2000)
+			);
+		}
+
+		function getCategories(classifieds) {
+			var categories = [];
+			angular.forEach(classifieds, function(item) {
+				angular.forEach(item.categories, function(category) {
+					categories.push(category);
+				});
+			});
+
+			return _.uniq(categories);
 		}
 	});
 }());
